b8b785c9f8ee98474e0cd9c4453b6445360c6319
ajsirh12 <ajsirh31@gmail.com>
2020-08-25 14:33:22 +0900

    multiprocessing calc

--- 0825/mulproc1.c
+++ 0825/mulproc1.c
@@ -78,7 +78,7 @@ void single_calc(){
     printf("data: %ld\ntime: %f\n", result, diff);
 }
 
-int main(){
+void dual_calc(){
     struct timeval begin, end;
     double diff;
     long result = 0;
@@ -118,7 +118,11 @@ int main(){
 
     diff = end.tv_sec + end.tv_usec / 1000000.0 - begin.tv_sec + begin.tv_usec / 1000000.0;
     printf("data: %ld\ntime: %f\n",data ,diff);
+}
 
+int main(){
+
+    dual_calc();
     single_calc();
     
     return 0;
